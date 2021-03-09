4d670dba627237b9452ab20dcbda88a3d154527c
Joe Lyons <josephjlyons90@gmail.com>
2021-03-09 14:29:48 -0500

    built table schema | no seeds yet just tracking

---
+++ db/employSchema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_managementDB;
+
+CREATE DATABASE employee_managementDB;
+
+USE employee_managementDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(25)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(25),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(15),
+    last_name VARCHAR(15),
+    manager_id INT,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
