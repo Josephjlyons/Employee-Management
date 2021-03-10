b4f3b8cba51d7aabe7c6be0f7ed7958a446e7695
Joe Lyons <josephjlyons90@gmail.com>
2021-03-10 15:30:38 -0500

    fixed issue that was causing issue in table+

--- db/employSchema.sql
+++ db/employSchema.sql
@@ -19,10 +19,10 @@ CREATE TABLE roles (
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    manager_id INT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
     role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -12,43 +12,43 @@ VALUE ('Enginerring');
 
 
 -- Employee Role Seeds --
-INSERT INTO role (title, salary, department_id)
-VALUE ('Lead Engineer' 165000, 4);
-INSERT INTO role (title, salary, department_id)
+
+INSERT INTO roles (title, salary, department_id)
+VALUE ('Lead Engineer', 165000, 4);
+INSERT INTO roles (title, salary, department_id)
 VALUE('Software Engineer', 135000, 4);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUE('Legal Team Lead', 225000, 2);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUE ('Lawyer', 175000, 2);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUE('Sales Lead', 120000, 1);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUE ('Salesperson', 85000, 1);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUE ('Accountant', 135000, 3);
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUE ('Payroll Admin', 110000, 3);
 
 
 -- Employee Seeds --
 -- Managers first --
-INSERT into employees (first_name, last_name, manger_id, role_id)
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUE ('Elvis', 'Stanley', null, 1);
-INSERT into employees (first_name, last_name, manger_id, role_id)
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUE ('Wanda', 'Dregler',null , 2 );
-INSERT into employees (first_name, last_name, manger_id, role_id)
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUE ('Shania', 'Burns', null , 3 );
-INSERT into employees (first_name, last_name, manger_id, role_id)
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUE ('Marvin', 'Steed', null , 4 );
 
 -- Employees that report to a manager --
-INSERT into employees (first_name, last_name, manger_id, role_id)
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUE ('Sting', 'McGee', 1 , 5 );
-INSERT into employees (first_name, last_name, manger_id, role_id)
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUE ('Randy', 'Mann', 2 , 6 );
-INSERT into employees (first_name, last_name, manger_id, role_id)
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUE ('Jose', 'Carloco', 3 , 7 );
-INSERT into employees (first_name, last_name, manger_id, role_id)
+INSERT INTO employees (first_name, last_name, manager_id, role_id)
 VALUE ('Harry', 'Backman', 4 , 8 );
-
 
